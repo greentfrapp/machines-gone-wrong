e058d8a53a92e415cc547b1d5339e9011d937541
Lim Swee Kiat <limsweekiat@gmail.com>
2019-09-01 19:07:26 +0800

    Small modifications for responsiveness

--- assets/js/guide_tofro.js
+++ assets/js/guide_tofro.js
@@ -7,5 +7,5 @@ let tofro = {
 	computed: {},
 	methods: {
 	},
-	template: '<div class="tofro"><hr/><div><div class="prev" v-if="prevtext"><a :href="prevlink"><i class="icon angle left"></i><span v-text="prevtext"></span></a></div><div class="next" v-if="nexttext"><a :href="nextlink"><span v-text="nexttext"></span><i class="icon angle right"></i></a></div></div><hr/></div>'
+	template: '<div class="tofro"><div class="prev" v-if="prevtext"><a :href="prevlink"><i class="icon angle left"></i><span v-text="prevtext"></span></a></div><div class="next" v-if="nexttext"><a :href="nextlink"><span v-text="nexttext"></span><i class="icon angle right"></i></a></div></div>'
 }
