9ddee0c872099b54a68083d2243d8c9c377dd3a5
SK <limsweekiat@gmail.com>
2022-02-17 22:38:36 +0800

    Add logging to messenger

--- assets/js/messenger.js
+++ assets/js/messenger.js
@@ -4,7 +4,13 @@ function messageHandler(event) {
   const { action, key, value } = event.data
   if (action == 'save'){
     window.localStorage.setItem(key, JSON.stringify(value))
+    console.log('saved')
+    console.log(event.data)
+    console.log('saved')
   } else if (action == 'get') {
+    console.log('get')
+    console.log(JSON.parse(window.localStorage.getItem(key)))
+    console.log('get')
     event.source.postMessage({
       action: 'returnData',
       key: key,
