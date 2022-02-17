2109d12bebf038727be6c65cd153099d322b4b7d
SK <limsweekiat@gmail.com>
2022-02-17 22:20:50 +0800

    Add messenger

---
+++ assets/js/messenger.js
@@ -0,0 +1,14 @@
+console.log('messenger loaded')
+window.addEventListener("message", messageHandler, false);
+function messageHandler(event) {
+  const { action, key, value } = event.data
+  if (action == 'save'){
+    window.localStorage.setItem(key, JSON.stringify(value))
+  } else if (action == 'get') {
+    event.source.postMessage({
+      action: 'returnData',
+      key,
+      JSON.parse(window.localStorage.getItem(key))
+    }, '*')
+  }
+}
